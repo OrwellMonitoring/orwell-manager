f7f7d60eea49be3c1032dcaebdb6ca4c74f5b643
VascoRegal <vascoregal24@gmail.com>
2022-05-09 16:55:16 +0100

    host page

--- dashboard/src/app/app-routing.module.ts
+++ dashboard/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { ImagesComponent } from './pages/images/images.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HostsComponent } from './pages/hosts/hosts.component';
 
 const routes: Routes = [
   { path: 'images', component: ImagesComponent },
+  { path: 'hosts', component: HostsComponent}
 ];
 
 @NgModule({
